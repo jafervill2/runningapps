7de29309772a6b52385e8bf233d42c43858e2872
jafervill2 <78231448+jafervill2@users.noreply.github.com>
2025-09-03 10:51:33 -0500

    Actualizar estrategiarunning.py

--- estrategiarunning.py
+++ estrategiarunning.py
@@ -98,7 +98,17 @@ if archivo is not None:
 
     # Conversión a min:seg
     df_interp["ritmo"] = (df_interp["ritmo_seg"] / 60).apply(lambda x: f"{int(x)}:{int((x%1)*60):02d}")
-    df_interp["tiempo_acum"] = (df_interp["tiempo_acum_seg"] / 60).apply(lambda x: f"{int(x)}:{int((x%1)*60):02d}")
+
+
+def format_hms(segundos):
+    h = int(segundos // 3600)
+    m = int((segundos % 3600) // 60)
+    s = int(segundos % 60)
+    return f"{h:02d}:{m:02d}:{s:02d}"
+
+    #df_interp["tiempo_acum_seg"] = df_interp["tiempo_seg"].cumsum()
+    df_interp["tiempo_acum"] = df_interp["tiempo_acum_seg"].apply(format_hms)
+    #df_interp["tiempo_acum"] = (df_interp["tiempo_acum_seg"] / 60).apply(lambda x: f"{int(x)}:{int((x%1)*60):02d}")
 
     # ============================
     # Mostrar resultados
