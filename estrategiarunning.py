3bb8eea76dc004f22ea4ef245392f7000ac00e84
jafervill2 <78231448+jafervill2@users.noreply.github.com>
2025-09-03 16:03:28 -0500

    Update estrategiarunning.py

--- estrategiarunning.py
+++ estrategiarunning.py
@@ -141,8 +141,8 @@ if archivo is not None:
 
 
 
-    tiempo_final = df_interp["tiempo_acum_seg"].iloc[-1]/60
-    ritmo_medio=format_ms((tiempo_final)/distancia_max}")
+    tiempo_final = df_interp["tiempo_acum_seg"].iloc[-1]/60 
+    ritmo_medio=format_hms((tiempo_final)/distancia_max})
     st.success(f"Tiempo estimado total para {distancia_opcion}: {tiempo_final}")
     st.success(f"Ritmo promedio para {distancia_opcion}: {ritmo_medio")
 
