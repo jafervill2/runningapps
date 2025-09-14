fd06787b4c08e8c725035eb3cf559324b74d39ae
jafervill2 <78231448+jafervill2@users.noreply.github.com>
2025-09-14 16:17:37 -0500

    Update estrategiarunning.py

--- estrategiarunning.py
+++ estrategiarunning.py
@@ -118,14 +118,14 @@ if archivo is not None:
     df_interp["tiempo_acum_seg"] = df_interp["tiempo_seg"].cumsum()
 
     # Formatos
-    df_interp["ritmo"] = (df_interp["ritmo_seg"] / 60).apply(lambda x: f"{00}:{int(x)}:{int((x%1)*60):02d}")
+   
 
     def format_hms(segundos):
         h = int(segundos // 3600)
         m = int((segundos % 3600) // 60)
         s = int(segundos % 60)
         return f"{h:02d}:{m:02d}:{s:02d}"
-
+    df_interp["ritmo"] = df_interp["ritmo"].apply(format_hms) #(df_interp["ritmo_seg"] / 60).apply(lambda x: f"{00}:{int(x)}:{int((x%1)*60):02d}")
     df_interp["tiempo_acum"] = df_interp["tiempo_acum_seg"].apply(format_hms)
     
     # ============================
