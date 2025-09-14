67cff76c6dca9913fc56cbf1cccc8f3e91ce20f8
jafervill2 <78231448+jafervill2@users.noreply.github.com>
2025-09-14 15:12:50 -0500

    Update estrategiarunning.py

--- estrategiarunning.py
+++ estrategiarunning.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 st.title("Simulador de Ritmo Ajustado por Pendiente y Altitud")
 
